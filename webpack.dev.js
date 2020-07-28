118f6663bb3d5400f8408d660c8681d7a2338ef6
Rene Juuse <rene.juuse@gmail.com>
2020-07-28 12:53:29 +0300

    feat: Different webpack configs for prod and dev

---
+++ webpack.dev.js
@@ -0,0 +1,23 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'source-map',
+  entry: './src/client/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        // Loaders are executed right-to-left (or bottom-to-top).
+        use: ['babel-loader', 'eslint-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/client/views/index.html',
+      filename: './index.html'
+    })
+  ]
+};
